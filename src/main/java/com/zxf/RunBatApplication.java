dc6b186a5de3892ecee1cfe5e9dd61d1c2fa3045
zhaoxiaofeng <1278442828@qq.com>
2019-07-02 17:24:07 +0800

    增加解析xml文件

--- src/main/java/com/zxf/RunBatApplication.java
+++ src/main/java/com/zxf/RunBatApplication.java
@@ -1,25 +1,29 @@
 package com.zxf;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.annotation.PostConstruct;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.util.ClassUtils;
-
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zxf.constains.Constains;
 import com.zxf.entity.RunBat;
 import com.zxf.utils.FileUtils;
-import com.zxf.utils.PathUtils;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 @SpringBootApplication
@@ -32,11 +36,76 @@ public class RunBatApplication extends SpringBootServletInitializer{
 	}
 	@PostConstruct//在初始化的时候初始化静态对象和它的静态成员变量bean对象，静态存储下来，防止被释放
     public void init() {
+		//this.runBat();
+		this.runXml();
+	}
+	
+	/**
+	 *  xml文件
+	 *  url: /run_bat/src/main/resources/static/RunXmlt.xml
+	 *  @author zhaoxiaofeng
+	 *  @deprecated：总体思路：DocumentBuilderFactory--->DocumentBuilder--->Document
+	 *  			 通过Document可获取所有的节点，拿到节点通过遍历可获取其属性和值
+	     *        加载并执行
+	 */
+	public void runXml() {
+		try {
+			String xmlFile = ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static/RunXml.xml";
+			logger.info("RunXml所在路劲："+xmlFile);
+			//获得DocumentBuilderFactory
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			//获得DocumentBuilder
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			//获得Document
+			Document document = builder.parse(new File(xmlFile));
+			NodeList productList = document.getElementsByTagName("Product");
+			//遍历productList
+			for(int i=0; i<productList.getLength(); i++) {
+				//通过 item(i)方法 获取一个Product节点，nodelist的索引值从0开始
+                Node Product = productList.item(i);
+                //获取Product节点的所有属性集合
+                NamedNodeMap attrs = Product.getAttributes();
+                //遍历属性
+                StringBuffer cmd = new StringBuffer();
+                cmd.append("cmd /k start ");
+                for(int j=0; j<attrs.getLength(); j++) {
+                	 //通过item(index)方法获取Product节点的某一个属性
+                    Node attr = attrs.item(j);
+                    //获取属性名和获取属性值
+                    logger.info("属性名：" + attr.getNodeName()+"------"+"属性值：" + attr.getNodeValue());
+                    if(j==1) {
+                    	cmd.append(attr.getNodeValue()+"/");// pass
+                    }else if(j==2) {
+                    	cmd.append(attr.getNodeValue());
+                    	logger.info(cmd.toString());
+                    	Runtime.getRuntime().exec(cmd.toString());
+                    }
+                }
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			logger.error(e.getMessage());
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			logger.error(e.getMessage());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			logger.error(e.getMessage());
+		}
+	}
+	/**
+	 *  json文件
+	 *  url: /run_bat/src/main/resources/static/RunBat.json
+	 *  @author zhaoxiaofeng
+	     *        加载并执行
+	 */
+	public void runBat() {
+
 		List<RunBat> list = new ArrayList<RunBat>();
 		try {
      		//String jsonFile = PathUtils.projectPath()+Constains.DataFile.DIR+Constains.DataFile.RUN_BAT_JSON;
 			String jsonFile = ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static/RunBat.json";
-			logger.error(jsonFile);
+			logger.info(jsonFile);
             String jsonStr = FileUtils.readFile(jsonFile);
             if(jsonStr == null || jsonStr.isEmpty()) {
             	logger.error("123456789"+jsonFile+"中没有任何可运行的bat文件！");
@@ -45,9 +114,9 @@ public class RunBatApplication extends SpringBootServletInitializer{
              	JavaType javaType = mapper.getTypeFactory().constructParametricType(ArrayList.class, RunBat.class);
              	list = mapper.readValue(jsonStr.getBytes("UTF-8"), javaType);
              	for(int i=0;i<list.size();i++) {
-             		String cmd = "cmd /k start "+list.get(i).getDir()+"\\"+list.get(i).getFile();// pass
-             		System.out.println(cmd);
-             		 Runtime.getRuntime().exec(cmd);
+             		String cmd = "cmd /k start "+list.get(i).getDir()+"/"+list.get(i).getFile();// pass
+             		logger.info(cmd);
+             		Runtime.getRuntime().exec(cmd);
              	}
             }
 		} catch (JsonParseException e) {
@@ -60,6 +129,7 @@ public class RunBatApplication extends SpringBootServletInitializer{
             // TODO Auto-generated catch block
         	logger.error(e.getMessage());
         }
+	
 	}
 
 	public static void main(String[] args) {
